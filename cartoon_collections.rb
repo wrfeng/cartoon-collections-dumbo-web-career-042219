7b6c7e9094a0f2ef2c6c515ce95a1e9318de1dc1
wei feng <theweifeng@gmail.com>
2019-04-12 01:33:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,7 @@ def summon_captain_planet(array)
   array.map {|ele| ele.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)
   # Your code here
 end
 

0dbde952cede5b464a540e7f91b3283f45e87220
wei feng <theweifeng@gmail.com>
2019-04-12 01:29:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(array)
-  array.map {|ele| ele.capitalize
+  array.map {|ele| ele.capitalize + "!"}
 end
 
 def long_planeteer_calls# code an argument here
